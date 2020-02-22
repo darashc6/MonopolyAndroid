5af2e0d8c0a85fcf6b452a527170606c2621500f
Darashc <darashc6@gmail.com>
2020-02-22 10:26:11 +0100

    Commit inicial
    Commit inicial

---
+++ desktop/src/com/darash/monopoly/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.darash.monopoly.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.darash.monopoly.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
